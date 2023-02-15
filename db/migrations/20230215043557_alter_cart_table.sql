e52f9f8c94c14dab363221fde4a84def2c88220d
Shinhye Kang <shnae.k@gmail.com>
2023-02-15 18:26:16 +0900

    [42기 강신혜] cart 테이블 size 열 수정]

--- db/migrations/20230215043557_alter_cart_table.sql
+++ db/migrations/20230215043557_alter_cart_table.sql
@@ -1,2 +1,4 @@
 -- migrate:up
+ALTER TABLE cart CHANGE size size_id int NOT NULL;
 ALTER TABLE cart ADD CONSTRAINT `cart_user_product_uq` UNIQUE (user_id, product_id);
+ALTER TABLE cart ADD CONSTRAINT `cart_size_id_fk` FOREIGN KEY (size_id) REFERENCES sizes (id);
